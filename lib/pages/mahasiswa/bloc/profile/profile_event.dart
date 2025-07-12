77bff8aaa102b154b634ddddab44c228093b7b92
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-07-13 00:09:56 +0700

    membuat event bloc mahasiswa untuk profile

--- lib/pages/mahasiswa/bloc/profile/profile_event.dart
+++ lib/pages/mahasiswa/bloc/profile/profile_event.dart
@@ -1,4 +1,12 @@
 part of 'profile_bloc.dart';
 
-@immutable
-sealed class ProfileEvent {}
+sealed class MahasiswaProfileEvent {}
+
+class LoadMahasiswaProfile extends MahasiswaProfileEvent {}
+
+class UpdateMahasiswaProfile extends MahasiswaProfileEvent {
+  final MahasiswaProfileRequestModel request;
+  UpdateMahasiswaProfile(this.request);
+}
+
+class LogoutMahasiswa extends MahasiswaProfileEvent {}
