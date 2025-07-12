7a0f6dc2cfba15077f7264816116f3e7f8416c64
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-07-13 00:10:44 +0700

    membuat bloc mahasiswa untuk fitur yang ada di profile

--- lib/pages/mahasiswa/bloc/profile/profile_bloc.dart
+++ lib/pages/mahasiswa/bloc/profile/profile_bloc.dart
@@ -1,13 +1,42 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:eventhelper_fe/data/model/request/mahasiswa/profile_request_model.dart';
+import 'package:eventhelper_fe/data/model/response/mahasiswa/profile_response_model.dart';
+import 'package:eventhelper_fe/data/repository/event_mahasiswa_repository.dart';
 
 part 'profile_event.dart';
 part 'profile_state.dart';
 
-class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  ProfileBloc() : super(ProfileInitial()) {
-    on<ProfileEvent>((event, emit) {
-      // TODO: implement event handler
+class MahasiswaProfileBloc
+    extends Bloc<MahasiswaProfileEvent, MahasiswaProfileState> {
+  final EventMahasiswaRepository mahasiswaRepository;
+
+  MahasiswaProfileBloc(this.mahasiswaRepository)
+    : super(MahasiswaProfileInitial()) {
+    on<LoadMahasiswaProfile>((event, emit) async {
+      emit(MahasiswaProfileLoading());
+      final result = await mahasiswaRepository.getProfile();
+      result.fold(
+        (e) => emit(MahasiswaProfileFailure(e)),
+        (profile) => emit(MahasiswaProfileLoaded(profile)),
+      );
+    });
+
+    on<UpdateMahasiswaProfile>((event, emit) async {
+      emit(MahasiswaProfileLoading());
+      final result = await mahasiswaRepository.updateProfile(event.request);
+      result.fold(
+        (e) => emit(MahasiswaProfileFailure(e)),
+        (msg) => emit(MahasiswaProfileUpdated(msg)),
+      );
+    });
+
+    on<LogoutMahasiswa>((event, emit) async {
+      emit(MahasiswaProfileLoading());
+      final result = await mahasiswaRepository.logout();
+      result.fold(
+        (e) => emit(MahasiswaProfileFailure(e)),
+        (msg) => emit(MahasiswaLogoutSuccess(msg)),
+      );
     });
   }
 }
